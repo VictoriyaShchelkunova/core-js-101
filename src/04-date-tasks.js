04b6d99997e305c601155208440d44d9637d1342
Victoriya Shchelkunova <victoriya.shchelcunova@gmail.com>
2020-12-10 22:57:25 +0300

    add solution 04-date-tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,12 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = new Date(date).getFullYear();
+  if ((!(year % 4) && year % 100) || (!(year % 100) && !(year % 400))) {
+    return true;
+  }
+  return false;
 }
 
 
@@ -73,8 +77,39 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const passedMilliseconds = endDate - startDate;
+
+  let dateComponents = [];
+
+  const hours = Math.trunc(passedMilliseconds / 3600000);
+  dateComponents.push(hours);
+
+  const minutes = Math.trunc((passedMilliseconds - hours * 3600000) / 60000);
+  dateComponents.push(minutes);
+
+  const seconds = Math.trunc((passedMilliseconds - hours * 3600000 - minutes * 60000) / 1000);
+  dateComponents.push(seconds);
+
+  const milliseconds = passedMilliseconds - hours * 3600000 - minutes * 60000 - seconds * 1000;
+  dateComponents.push(milliseconds);
+
+  dateComponents = dateComponents.map((dateComponent, index, array) => {
+    if (dateComponent < 10 && index !== array.length - 1) {
+      return `0${dateComponent}`;
+    }
+    if (index === array.length - 1) {
+      if (dateComponent < 10) {
+        return `00${milliseconds}`;
+      }
+      if (dateComponent > 9 && dateComponent < 100) {
+        return `0${milliseconds}`;
+      }
+    }
+    return dateComponent;
+  });
+
+  return `${dateComponents[0]}:${dateComponents[1]}:${dateComponents[2]}.${dateComponents[3]}`;
 }
 
 
@@ -94,8 +129,19 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const dateRes = new Date(date);
+  let angle = 0.5 * (60 * dateRes.getUTCHours() - 11 * dateRes.getUTCMinutes());
+  if (angle > 180) {
+    angle = 360 - angle;
+    if (angle < 0) {
+      angle *= -1;
+    }
+    if (angle > 180) {
+      angle = 360 - angle;
+    }
+  }
+  return (Math.PI / 180) * angle;
 }
 
 
